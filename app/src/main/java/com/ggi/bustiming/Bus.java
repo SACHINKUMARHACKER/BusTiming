f4f2f77b30a128d83f83bb6e4c273e57c35c3e4f
SACHINKUMARHACKER <sachinkum123567@gmail.com>
2019-04-12 21:57:02 +0530

    Initial commit

---
+++ app/src/main/java/com/ggi/bustiming/Bus.java
@@ -0,0 +1,27 @@
+package com.ggi.bustiming;
+
+public class Bus {
+    private String place;
+    private String time;
+    public Bus(String Place, String Time){
+        place=Place;
+        time=Time;
+
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ app/src/main/java/com/ggi/bustiming/LdhActivity.java
@@ -0,0 +1,56 @@
+package com.ggi.bustiming;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class LdhActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ldh);
+
+        ArrayList<Bus> busArrayList = new ArrayList<>();
+
+
+        Bus b1 = new Bus("Samarala chowk","7:45 am");
+        Bus b2 = new Bus("Samarala chowk","8:10 am");
+        Bus b3 = new Bus("Samarala chowk","8:50 am");
+        Bus b4 = new Bus("Samarala chowk","9:30 am");
+        Bus b5 = new Bus("Sherpur chowk","8:25 am");
+        Bus b6 = new Bus("Sherpur chowk","8:35 am");
+        Bus b7 = new Bus("Sherpur chowk","9:00 am");
+        Bus b8 = new Bus("Sherpur chowk","7:45 am");
+        Bus b9 = new Bus("Sherpur chowk","7:45 am");
+        Bus b10 = new Bus("Sherpur chowk","7:45 am");
+
+        busArrayList.add(b1);
+        busArrayList.add(b2);
+        busArrayList.add(b3);
+        busArrayList.add(b4);
+        busArrayList.add(b5);
+        busArrayList.add(b6);
+        busArrayList.add(b7);
+        busArrayList.add(b8);
+        busArrayList.add(b9);
+        busArrayList.add(b10);
+
+
+        BusAdapter adapter = new BusAdapter(getApplicationContext(),busArrayList);
+        ListView listView= findViewById(R.id.bus_view);
+        listView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(LdhActivity.this, BeginActivity.class));
+        finish();
+        super.onBackPressed();
+    }
+}
